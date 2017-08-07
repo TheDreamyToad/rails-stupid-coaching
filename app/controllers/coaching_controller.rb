5967a9d017bf56c5c68fabff9a714dbb1514a8ca
Jean Daniel DULOIR <jdduloir@icloud.com>
2017-08-07 14:56:14 +0200

    COACHING

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -5,16 +5,15 @@ class CoachingController < ApplicationController
   def answer
     @answer = params[:asker]
   # TODO: return coach answer to your_message
-    if ask.include? "?"
-      return "Silly question, get dressed and go to work!"
-    elsif your_message == "I am going to work right now!"
-      return ""
-    elsif your_message == "I AM GOING TO WORK RIGHT NOW!"
-      return ""
-    elsif your_message == your_message.upcase
-      return "I can feel your motivation!" + " " + coach_answer(your_message)
-   else
-      return "I don't care, get dressed and go to work!"
+    if @answer.include? "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+    elsif @answer == "I am going to work right now!"
+      @coach_answer = ""
+    elsif @answer == "I AM GOING TO WORK RIGHT NOW!"
+      @coach_answer = ""
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
     end
+    return @coach_answer
   end
 end
